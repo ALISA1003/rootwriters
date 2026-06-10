c12f82741b14b5f119d0fb4e233a0f3c8e796697
ALISA1003 <alexanderisaev@bk.ru>
2026-06-10 14:49:29 +0300

    adr fix v4

--- rootwriters.c
+++ rootwriters.c
@@ -360,14 +360,7 @@ static int install_hook(struct ftrace_hook *hook)
 
 	hook->ops.func = rootwriters_ftrace_thunk;
 	
-	/* Используем макрос, который требует сохранение регистров только если архитектура это умеет.
-	 * На многих arm64 FTRACE_OPS_FL_SAVE_REGS с IPMODIFY может возвращать -EINVAL.
-	 */
-#ifndef FTRACE_OPS_FL_SAVE_REGS_IF_SUPPORTED
-#define FTRACE_OPS_FL_SAVE_REGS_IF_SUPPORTED FTRACE_OPS_FL_SAVE_REGS
-#endif
-
-	hook->ops.flags = FTRACE_OPS_FL_SAVE_REGS_IF_SUPPORTED | FTRACE_OPS_FL_IPMODIFY | FTRACE_OPS_FL_RECURSION_SAFE;
+    hook->ops.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY;
 
 	ret = ftrace_set_filter_ip(&hook->ops, ftrace_addr, 0, 0);
 	if (ret)
